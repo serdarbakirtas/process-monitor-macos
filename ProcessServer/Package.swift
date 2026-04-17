faf76b48a4722f807903b8cdd287def17fadd954
Hasan Serdar Bakirtas <serdarbakirtas@gmail.com>
2026-04-18 01:46:56 +0200

    Initialize ProcessServer project and generated gRPC code

---
+++ ProcessServer/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "ProcessServer",
+    platforms: [
+        .macOS(.v13)
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/grpc/grpc-swift.git",
+            from: "1.23.0"
+        ),
+    ],
+    targets: [
+        .executableTarget(
+            name: "ProcessServer",
+            dependencies: [
+                .product(name: "GRPC", package: "grpc-swift"),
+            ],
+            path: "Sources/ProcessServer"
+        ),
+    ]
+)
